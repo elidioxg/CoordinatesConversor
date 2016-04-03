42afd53b880c737e223288eec71a4b7936d033e5
elidioxg <elidioxg@gmail.com>
2016-04-02 21:56:57 -0300

    Controller changed

--- src/geoconversor/controller/AppController.java
+++ src/geoconversor/controller/AppController.java
@@ -3,7 +3,12 @@ package geoconversor.controller;
 
 import geoconversor.conversion.CoordinateConversion;
 import geoconversor.conversion.DMSConversion;
+import geoconversor.conversion.ValidateConversion;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.fxml.FXML;
+import javafx.scene.AccessibleAttribute;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 public class AppController {
@@ -26,7 +31,7 @@ public class AppController {
         String strSector  = tfSector.getCharacters().toString();        
         String strNorth = tfNorth.getCharacters().toString().trim();
         String strEast = tfEast.getCharacters().toString().trim();
-        double latlon[] = convert(strSector, strNorth,strEast);                
+        double latlon[] = convert(strSector, strNorth,strEast);               
         tfLatitude.setText(String.format(" %.4f", latlon[0]));
         tfLongitude.setText(String.format(" %.4f", latlon[1]));
     }
@@ -40,35 +45,74 @@ public class AppController {
         tfEast.setText(strUtm[3]+" m");
     }    
     
+    @FXML protected RadioButton rbLatitude;
+    @FXML protected RadioButton rbLongitude;
     @FXML 
     protected void convertDMS(){        
         DMSConversion dms = new DMSConversion();
         String latDeg = tfLatDeg.getText();
         String latMin = tfLatMin.getText();
         String latSec = tfLatSec.getText();
-        // TO DO: implement checkbox with N/S and E/W
-        boolean latSignal = true;
-        tfLatitude.setText(dms.convertToDegrees(latSignal, latDeg, latMin, latSec));
         String lonDeg = tfLonDeg.getText();
         String lonMin = tfLonMin.getText();
         String lonSec = tfLonSec.getText();
+        boolean latSignal = true;
+        if(!rbLatitude.isSelected()){ 
+            latSignal = false;
+        }        
         boolean lonSignal = true;
-        tfLongitude.setText(dms.convertToDegrees(lonSignal, lonDeg, lonMin, lonSec));        
+        if(!rbLongitude.isSelected()){
+            lonSignal = false;
+        }
+        List list = new ArrayList<>();
+        list = convert(latSignal,latDeg, latMin, latSec, lonSignal,lonDeg, lonMin, lonSec);        
+        tfLatitude.setText(list.get(0).toString());       
+        tfLongitude.setText(list.get(1).toString());
     }
     private String[] convert(String lat, String lon){
-        CoordinateConversion cc = new CoordinateConversion();
-        String utm = cc.latLon2UTM(Double.parseDouble(lat),
-                Double.parseDouble(lon));
-        utm = utm.replace(",", ".");
-        String strUtm[] = utm.split(" ");
-        return strUtm;
+        lat = lat.replace(",", ".");
+        lon = lon.replace(",", ".");
+        double latitude = Double.parseDouble(lat);
+        double longitude = Double.parseDouble(lon);
+        ValidateConversion vc = new ValidateConversion();
+        int error = vc.validate(latitude, longitude);
+        if(error==0){
+            CoordinateConversion cc = new CoordinateConversion();
+            String utm = cc.latLon2UTM(latitude, longitude);
+            utm = utm.replace(",", ".");
+            String strUtm[] = utm.split(" ");
+            return strUtm;
+        }
+        return null;
     }
     private double[] convert(String sector, String north, String east){
-        String strSec[] = sector.split(" ");
-        String strUtm =strSec[0]+" "+strSec[1]+" "+north+" "+east;        
-        CoordinateConversion cc = new CoordinateConversion();
-        double[] latlon = cc.utm2LatLon(strUtm);            
-        return latlon;
+        ValidateConversion vc = new ValidateConversion();
+        int error = vc.validate(sector, north, east);
+        if(error ==0){
+            String strSec[] = sector.split(" ");
+            String strUtm =strSec[0]+" "+strSec[1]+" "+north+" "+east;        
+            CoordinateConversion cc = new CoordinateConversion();
+            double[] latlon = cc.utm2LatLon(strUtm);            
+            return latlon;
+        }
+        return null;
     }
-    
+
+    private ArrayList convert(boolean latSignal, String latGrau, String latMin, 
+            String latSec, boolean lonSignal, String lonGrau, String lonMin, 
+            String lonSec){
+        ValidateConversion vc = new ValidateConversion();
+        int error = vc.validate(latGrau, latMin, latSec, lonGrau, lonMin, lonSec);
+        if(error == 0){
+            DMSConversion dms = new DMSConversion();
+            String latitude = dms.convertToDegrees(latSignal, latGrau, latMin, latSec);                      
+            String longitude = dms.convertToDegrees(lonSignal, lonGrau, lonMin, lonSec);    
+            List aux = new ArrayList<>();
+            aux.add(latitude);
+            aux.add(longitude);
+            return (ArrayList)aux;
+            
+        }        
+        return null;
+    }
 }
